5fe000d2ec103d29848a3e050e520fa127485e12
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-09-14 21:25:06 -0300

    add color sass and improve chatbot

--- src/Components/Chatbot/ChatMessage/ChatMessage.jsx
+++ src/Components/Chatbot/ChatMessage/ChatMessage.jsx
@@ -5,73 +5,86 @@ import chatAvatar from '../../../assets/images/chat-avatar.svg';
 import API from '../../../settings/API';
 
 export default function ChatMessage() {
-    const [expanded, setExpanded] = useState(false);
-    const [userMessage, setUserMessage] = useState('');
-    const [chatMessages, setChatMessages] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+  const [userMessage, setUserMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
+  const [chatHistory, setChatHistory] = useState([]);
 
-    const handleSize = () => {
-        setExpanded(!expanded);
-    };
+  useEffect(() => {
+    const chatHistoryFromLocalStorage = localStorage.getItem('chatHistory');
+    if (chatHistoryFromLocalStorage) {
+      const chatHistoryArray = JSON.parse(chatHistoryFromLocalStorage);
+      setChatHistory(chatHistoryArray);
+    }
+  }, []);
 
-    const handleChat = async () => {
-        const trimmedMessage = userMessage.trim();
+  const handleSize = () => {
+    setExpanded(!expanded);
+  };
 
-        if (!trimmedMessage) {
-            return;
-        }
-
-        setUserMessage("");
-
-        const response = await API.post("/api/chat", { message: trimmedMessage });
+  const handleChat = () => {
+    API
+      .post("/api/chat", {
+        message: userMessage,
+      })
+      .then((response) => {
+        setChatMessages([...chatMessages, {
+          type: "chat__user",
+          message: userMessage,
+          alignment: "right",
+        }]);
 
         setChatMessages([...chatMessages, {
-            message: response.data.message,
-            type: "incoming"
+          type: "chat__bot",
+          message: response.data.message,
+          alignment: "left",
+          icon: chatAvatar,
         }]);
-    };
 
-    useEffect(() => {
-        API
-            .get("/api/chat")
-            .then(response => {
-                setChatMessages(response.data.messages);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, []);
+        const newChatHistory = [...chatHistory, {
+          message: userMessage,
+          date: new Date(),
+        }];
+        setChatHistory(newChatHistory);
 
-    return (
-        <div className="chat__message" style={{ height: expanded ? 500 : 409 }}>
-            <div className="chat__container">
-                <header className="chat__header">
-                    <img src={chatAvatar} alt="Chat Avatar" />
-                    <button onClick={handleSize} className="chat__button">
-                        <p className="chat__text">{expanded ? "Recolher" : "Expandir"}</p>
-                        <span className="chat__icon">{expanded ? <BiCollapseAlt /> : <BiExpandAlt />}</span>
-                    </button>
-                </header>
+        localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-                <div className="chat__hello">
-                    <h1 className="chat__title">🖖🏾 Como posso ajudar?</h1>
-                </div>
+  return (
+    <div className="chat__message" style={{ height: expanded ? 500 : 409 }}>
+      <div className="chat__container">
+        <header className="chat__header">
+          <img src={chatAvatar} alt="Chat Avatar" />
+          <button onClick={handleSize} className="chat__button">
+            <p className="chat__text">{expanded ? "Recolher" : "Expandir"}</p>
+            <span className="chat__icon">{expanded ? <BiCollapseAlt /> : <BiExpandAlt />}</span>
+          </button>
+        </header>
 
-                <ul className="chat__box" style={{ height: expanded ? 270 : 180 }}>
-                    <h1 className="chat__placebox">Role para cima ⤴︎ para ver o histórico</h1>
-                    {chatMessages?.map((message, index) => (
-                        <li key={index} className={message.type}>
-                            {message.message}
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="chat__input">
-                    <input type="text" className="chat__text text--change" placeholder="Digite sua dúvida" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} />
-                    <button className="chat__send send--change" onClick={handleChat}>
-                        <FiSend className="chat__icon" />
-                    </button>
-                </div>
-            </div>
+        <div className="chat__hello">
+          <h1 className="chat__title">🖖🏾 Como posso ajudar?</h1>
         </div>
-    )
+
+        <ul className="chat__box" style={{ height: expanded ? 270 : 180 }}>
+          {chatMessages?.map((message, index) => (
+            <li key={index} className={message.type}>
+              {message.message}
+              {message.date.toLocaleDateString()}
+            </li>
+          ))}
+        </ul>
+
+        <div className="chat__input">
+          <input type="text" className="chat__text text--change" placeholder="Digite sua dúvida" value={userMessage} onChange={(e) => setUserMessage(e.target.value)} />
+          <button className="chat__send send--change" onClick={handleChat}>
+            <FiSend className="chat__icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
